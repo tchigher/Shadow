b51c00826c99888d34e0fc31b48295aa2cf6fc43
王鹏文 <wangpengwen@migu.cn>
2021-01-15 00:30:51 +0800

    简化

--- sample/sample-manager/src/main/java/com/tencent/shadow/sample/manager/SamplePluginManager.java
+++ sample/sample-manager/src/main/java/com/tencent/shadow/sample/manager/SamplePluginManager.java
@@ -13,12 +13,11 @@ import com.tencent.shadow.sample.constant.Constant;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 public class SamplePluginManager extends FastPluginManager {
 
-    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final ExecutorService mExecutorService = Executors.newSingleThreadExecutor();
 
-    private Context mCurrentContext;
+    private final Context mCurrentContext;
 
     public SamplePluginManager(Context context) {
         super(context);
@@ -26,7 +25,7 @@ public class SamplePluginManager extends FastPluginManager {
     }
 
     /**
-     * @return PluginManager实现的别名，用于区分不同PluginManager实现的数据存储路径
+     * @return PluginManager 实现的别名，用于区分不同 PluginManager 实现的数据存储路径
      */
     @Override
     protected String getName() {
@@ -34,7 +33,7 @@ public class SamplePluginManager extends FastPluginManager {
     }
 
     /**
-     * @return 宿主so的ABI。插件必须和宿主使用相同的ABI。
+     * @return 宿主 so 的 ABI. 插件必须和宿主使用相同的 ABI
      */
     @Override
     public String getAbi() {
@@ -42,67 +41,80 @@ public class SamplePluginManager extends FastPluginManager {
     }
 
     /**
-     * @return 宿主中注册的PluginProcessService实现的类名
+     * @return 宿主中注册的 PluginProcessService 实现的类名
      */
     @Override
     protected String getPluginProcessServiceName(String partKey) {
-        if ("sample-plugin-app".equals(partKey)) {
-            return "com.tencent.shadow.sample.host.PluginProcessPPS";
+        if ("sample-plugin-app1".equals(partKey)) {
+            return "com.tencent.shadow.sample.host.Plugin1ProcessPPS";
         } else if ("sample-plugin-app2".equals(partKey)) {
-            return "com.tencent.shadow.sample.host.Plugin2ProcessPPS";//在这里支持多个插件
+            return "com.tencent.shadow.sample.host.Plugin2ProcessPPS"; // 在这里支持多个插件
         } else {
-            //如果有默认PPS，可用return代替throw
+            // 如果有默认 PPS，可用 return 代替 throw
             throw new IllegalArgumentException("unexpected plugin load request: " + partKey);
         }
     }
 
     @Override
-    public void enter(final Context context, long fromId, Bundle bundle, final EnterCallback callback) {
+    public void enter(
+            final Context context,
+            long fromId,
+            Bundle bundle,
+            final EnterCallback callback
+    ) {
         if (fromId == Constant.FROM_ID_NOOP) {
-            //do nothing.
+            // do nothing
         } else if (fromId == Constant.FROM_ID_START_ACTIVITY) {
             onStartActivity(context, bundle, callback);
         } else {
-            throw new IllegalArgumentException("不认识的fromId==" + fromId);
+            throw new IllegalArgumentException("不认识的 fromId == " + fromId);
         }
     }
 
-    private void onStartActivity(final Context context, Bundle bundle, final EnterCallback callback) {
-        final String pluginZipPath = bundle.getString(Constant.KEY_PLUGIN_ZIP_PATH);
-        final String partKey = bundle.getString(Constant.KEY_PLUGIN_PART_KEY);
-        final String className = bundle.getString(Constant.KEY_ACTIVITY_CLASSNAME);
-        if (className == null) {
-            throw new NullPointerException("className == null");
+    private void onStartActivity(
+            final Context context,
+            Bundle bundle,
+            final EnterCallback callback
+    ) {
+        final String pluginsZipFileAbsolutePath = bundle.getString(Constant.KEY__PLUGINS_ZIP_FILE__ABSOLUTE_PATH);
+        final String targetPluginApp = bundle.getString(Constant.KEY__TARGET_PLUGIN_APP);
+        final String targetPluginActivityClassFullName = bundle.getString(Constant.KEY__TARGET_PLUGIN_ACTIVITY__CLASS_FULL_NAME);
+        if (targetPluginActivityClassFullName == null) {
+            throw new NullPointerException("classFullName == null");
         }
-        final Bundle extras = bundle.getBundle(Constant.KEY_EXTRAS);
+
+        final Bundle targetPluginActivityIntentExtras = bundle.getBundle(Constant.KEY__TARGET_PLUGIN_ACTIVITY__INTENT_EXTRAS);
 
         if (callback != null) {
             final View view = LayoutInflater.from(mCurrentContext).inflate(R.layout.activity_load_plugin, null);
             callback.onShowLoadingView(view);
         }
 
-        executorService.execute(new Runnable() {
+        mExecutorService.execute(new Runnable() {
             @Override
             public void run() {
                 try {
-                    InstalledPlugin installedPlugin = installPlugin(pluginZipPath, null, true);
-                    Intent pluginIntent = new Intent();
-                    pluginIntent.setClassName(
+                    InstalledPlugin installedPlugins = installPlugin(pluginsZipFileAbsolutePath, null, true);
+
+                    Intent targetPluginActivityIntent = new Intent();
+                    targetPluginActivityIntent.setClassName(
                             context.getPackageName(),
-                            className
+                            targetPluginActivityClassFullName
                     );
-                    if (extras != null) {
-                        pluginIntent.replaceExtras(extras);
+                    if (targetPluginActivityIntentExtras != null) {
+                        targetPluginActivityIntent.replaceExtras(targetPluginActivityIntentExtras);
                     }
 
-                    startPluginActivity(installedPlugin, partKey, pluginIntent);
+                    startPluginActivity(installedPlugins, targetPluginApp, targetPluginActivityIntent);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
+
                 if (callback != null) {
                     callback.onCloseLoadingView();
                 }
             }
         });
     }
+
 }
