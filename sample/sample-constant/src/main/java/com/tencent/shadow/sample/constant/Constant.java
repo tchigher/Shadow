8e6f5f80e91f15227a3405d9f65dd0f53ce6e60e
王鹏文 <wangpengwen@migu.cn>
2021-01-14 23:40:48 +0800

    简化

--- sample/sample-constant/src/main/java/com/tencent/shadow/sample/constant/Constant.java
+++ sample/sample-constant/src/main/java/com/tencent/shadow/sample/constant/Constant.java
@@ -1,12 +1,12 @@
 package com.tencent.shadow.sample.constant;
 
 final public class Constant {
-    public static final String KEY_PLUGIN_ZIP_PATH = "pluginZipPath";
-    public static final String KEY_ACTIVITY_CLASSNAME = "KEY_ACTIVITY_CLASSNAME";
-    public static final String KEY_EXTRAS = "KEY_EXTRAS";
-    public static final String KEY_PLUGIN_PART_KEY = "KEY_PLUGIN_PART_KEY";
-    public static final String PART_KEY_PLUGIN_MAIN_APP = "sample-plugin-app";
-    public static final String PART_KEY_PLUGIN_ANOTHER_APP = "sample-plugin-app2";
+    public static final String KEY__PLUGINS_ZIP_FILE__ABSOLUTE_PATH = "KEY__PLUGINS_ZIP_FILE__ABSOLUTE_PATH";
+    public static final String KEY__TARGET_PLUGIN_ACTIVITY__CLASS_FULL_NAME = "KEY__TARGET_PLUGIN_ACTIVITY__CLASS_FULL_NAME";
+    public static final String KEY__TARGET_PLUGIN_ACTIVITY__INTENT_EXTRAS = "KEY__TARGET_PLUGIN_ACTIVITY__INTENT_EXTRAS";
+    public static final String KEY__TARGET_PLUGIN_APP = "KEY__TARGET_PLUGIN_APP";
+    public static final String KEY__TARGET_PLUGIN_APP__ONE = "sample-plugin-app1";
+    public static final String KEY__TARGET_PLUGIN_APP__TWO = "sample-plugin-app2";
 
     public static final int FROM_ID_NOOP = 1000;
     public static final int FROM_ID_START_ACTIVITY = 1002;
