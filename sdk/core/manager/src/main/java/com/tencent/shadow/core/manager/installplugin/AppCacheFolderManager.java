528434ff8969164b0217903a032cd0fdf72a9301
王鹏文 <wangpengwen@migu.cn>
2021-01-15 10:50:23 +0800

    简化

--- sdk/core/manager/src/main/java/com/tencent/shadow/core/manager/installplugin/AppCacheFolderManager.java
+++ sdk/core/manager/src/main/java/com/tencent/shadow/core/manager/installplugin/AppCacheFolderManager.java
@@ -7,39 +7,65 @@ import java.io.File;
  */
 public class AppCacheFolderManager {
 
-    public static File getVersionDir(File root, String appName, String version) {
-        return new File(getAppDir(root, appName), version);
+    public static File getVersionedPluginsUnpackDirInNamedPluginsManagersDir(
+            File pluginsUnpackDir,
+            String mPluginsManagerName,
+            String pluginsZipFileHash
+    ) {
+        return new File(
+                getNamedPluginsManagerDirInPluginsUnpackDir(pluginsUnpackDir, mPluginsManagerName),
+                pluginsZipFileHash
+        );
     }
 
-    public static File getAppDir(File root, String appName) {
-        return new File(root, appName);
-    }
-
-
-    public static File getODexDir(File root,String key){
-        return new File(getODexRootDir(root), key+ "_odex");
-    }
-
-    public static File getODexCopiedFile(File oDexDir,String key){
-        return new File(oDexDir, key+ "_oDexed");
-    }
-
-
-    private static File getODexRootDir(File root){
-        return new File(root,"oDex");
-    }
-
-    public static File getLibDir(File root,String key){
-        return new File(getLibRootDir(root), key+ "_lib");
-    }
-
-    public static File getLibCopiedFile(File soDir,String key){
-        return new File(soDir, key+ "_copied");
+    public static File getNamedPluginsManagerDirInPluginsUnpackDir(
+            File pluginsUnpackDir,
+            String pluginsManagerName
+    ) {
+        return new File(pluginsUnpackDir, pluginsManagerName);
     }
 
 
-    private static File getLibRootDir(File root){
-        return new File(root,"lib");
+    public static File getOdexDir(
+            File root,
+            String key
+    ) {
+        return new File(getOdexRootDir(root), key + "_odex");
+    }
+
+    public static File getOdexCopiedTagFile(
+            File odexDir,
+            String pluginAppPartKey
+    ) {
+        return new File(odexDir, pluginAppPartKey + "_odexed");
+    }
+
+
+    private static File getOdexRootDir(
+            File root
+    ) {
+        return new File(root, "oDex");
+    }
+
+    public static File getLibDir(
+            File root,
+            String key
+    ) {
+        return new File(getLibRootDir(root), key + "_lib");
+    }
+
+    public static File getLibCopiedFile(
+            File soDir,
+            String key
+    ) {
+        return new File(soDir, key + "_copied");
+    }
+
+
+    private static File getLibRootDir(
+            File root
+    ) {
+        return new File(root, "lib");
     }
 
 }
